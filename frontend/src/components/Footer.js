f0a1b27dd01c98686dbadd55890b0fa9d1d4e334
raghavg27 <raghavg2705@gmail.com>
2024-06-07 16:12:38 +0530

    fixed mobile vieww

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,7 +9,6 @@ function Footer() {
       textAlign="center"
       bgcolor="primary.main"
       color="text.primary"
-      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
     >
       <Typography variant="body1">
         © 2024 minimaLINK. All rights reserved.

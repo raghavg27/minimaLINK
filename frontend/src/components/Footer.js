947faf422791b377fdc5b6fd689e040b0b57f972
raghavg27 <raghavg2705@gmail.com>
2024-06-07 16:27:35 +0530

    fixed footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
 function Footer() {
   return (
-    <Box
-      mt={5}
-      py={3}
-      textAlign="center"
-      bgcolor="primary.main"
-      color="text.primary"
-    >
-      <Typography variant="body1">
+    <AppBar position="static" sx={{ boxShadow: 3, backgroundColor: '#333' }}>
+      <Toolbar>
+        <Box width="100%" display="flex" justifyContent="center" mt={5} py={3} textAlign="center" bgcolor="primary.main" color="text.primary">
+          <Typography variant="body1">
         © 2024 minimaLINK. All rights reserved.
       </Typography>
-    </Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
 

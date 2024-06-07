75a9e3ba27b250694f82b7da8b591816b18e0a97
raghavg27 <raghavg2705@gmail.com>
2024-06-07 16:35:22 +0530

    fixed foot

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ import { Box } from '@mui/system';
 
 function Footer() {
   return (
-    <AppBar position="static" sx={{ boxShadow: 3, backgroundColor: '#333' }}>
+    
       <Toolbar>
         <Box width="100%" display="flex" justifyContent="center" mt={5} py={3} textAlign="center" bgcolor="primary.main" color="text.primary">
           <Typography variant="body1">
@@ -12,7 +12,7 @@ function Footer() {
       </Typography>
         </Box>
       </Toolbar>
-    </AppBar>
+    
   );
 }
 

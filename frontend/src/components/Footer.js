0b0024215e614f5c473ef06fc30e6cd567af6cee
raghavg27 <raghavg2705@gmail.com>
2024-06-09 18:56:12 +0530

    circular progress icon

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
       <Toolbar>
         <Box width="100%" display="flex" justifyContent="center" mt={5} py={3} textAlign="center" bgcolor="primary.main" color="text.primary">
           <Typography variant="body1">
-            © 2024 minimaLINK. All rights reserved. <br />
+            <a href="https://github.com/raghavg27/minimaLINK" target="_blank" > Project repository </a> <br />
             NOTE: Backend server will spin down with inactivity, which can delay requests by 50 seconds or more. <br /> 
               Please refresh page and wait till instance wakes up :(
           </Typography>

e7e362aa0a22812df8904727affc8a8b121f89ea
raghavg27 <raghavg2705@gmail.com>
2024-07-27 02:34:53 +0530

    last5api update

--- server.js
+++ server.js
@@ -184,7 +184,9 @@ app.get('/api/v1/data/last5', authenticateJWT, async (req, res) => {
     const links = result.rows.map(row => ({
       shortLink: `${url}/${row.short_url}`,
       originalLink: row.long_url,
-      dateCreated: formatDate(row.created_at)
+      dateCreated: formatDate(row.created_at),
+      shortLinkId: row.id,
+      clicks: row.clicks
     }));
 
     res.json(links);
